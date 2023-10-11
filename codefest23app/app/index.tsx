594db375b28f65fc36dbd1b71f244be963917924
diablolaw <94478511+diablolaw@users.noreply.github.com>
2023-10-11 20:49:30 +0100

    add routing

---
+++ codefest23app/app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text, Button, useThemeMode } from "@rneui/themed";
+
+export default function login() {
+  return (
+    <View>
+      <Text h3>login window</Text>
+    </View>
+  );
+}
