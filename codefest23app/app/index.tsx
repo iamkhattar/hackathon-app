4019047bde5778f7034cd36f11128976b2695643
diablolaw <94478511+diablolaw@users.noreply.github.com>
2023-10-11 23:39:08 +0100

    appointment details tile with map

--- codefest23app/app/index.tsx
+++ codefest23app/app/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import { Text, Button, useThemeMode } from "@rneui/themed";
 import AppointmentItem from "../components/AppointmentItem";
+import AppointmentDetailsTile from "../components/AppointmentDetailsTile";
 
 export default function login() {
   return (
@@ -13,6 +14,17 @@ export default function login() {
           startTime: 1697056755,
           endTime: 1697063955,
           state: 1,
+        }}
+      />
+      <AppointmentDetailsTile
+        appointment={{
+          clientName: "Tom Williams",
+          startTime: 1697056755,
+          endTime: 1697063955,
+          state: 1,
+          lat: 55.215838,
+          long: -4.441463,
+          id: 123,
         }}
       />
     </View>
